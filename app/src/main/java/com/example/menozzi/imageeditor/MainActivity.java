0d95f4c7f70467551cf88f7b2f65ec9fa9816abb
Neil Davis <neilbd@live.unc.edu>
2017-04-05 11:10:03 -0400

    Added menu ability and implemented Camera intent

--- app/src/main/java/com/example/menozzi/imageeditor/MainActivity.java
+++ app/src/main/java/com/example/menozzi/imageeditor/MainActivity.java
@@ -1,10 +1,25 @@
 package com.example.menozzi.imageeditor;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import static android.R.attr.x;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -16,9 +31,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
 
         /*
 
@@ -28,30 +64,86 @@ public class MainActivity extends AppCompatActivity {
          */
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cam, 1);
+        } else if (id == R.id.blur) {
+
+        } else if (id == R.id.grayscale) {
+
+        } else if (id == R.id.filter) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent res){
+
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = res.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView img = (ImageView) findViewById(R.id.image);
+            img.setImageBitmap(imageBitmap);
+        }
+
+    }
+
+    public native String stringFromJNI();
+
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    //
 
 
     /*
 
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent res){
 
-            if(requestCode == 1 && resultCode == RESULT_OK){
-                Bundle extras = x.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                img = (ImageView) findViewById(R.id.edit_image);
-                img.setImageBitmap(imageBitmap);
-            }
-
-        }
 
         public void newPicture(View v){
-            Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(cam, 1);
+
 
         }
 
